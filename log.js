f38e7353f642442700cc140a05cc79e472e4c29f
WilliamSuarez2006 <ws4389495@gmail.com>
2025-03-17 09:23:34 -0500

    Subiendo cambios JS

--- log.js
+++ log.js
@@ -1,44 +1,59 @@
-
-    let entrada="";
-    let codigo=0;
-    let binario="";
-
-function ejecutar(){
+let entrada = "";
+let codigo = 0;
+let binario = "";
 
 
-    entrada=document.getElementById("entrada").value;
-    codigo=entrada.charCodeAt(0);
-    document.getElementById("salida").innerText="El caracter "+entrada+" en codigo ASCCI es:"+codigo;
-    document.getElementById("car-bin").value=codigo;
+function ejecutar() {
+    entrada = document.getElementById("entrada").value;
+    codigo = entrada.charCodeAt(0);
+    document.getElementById("salida").innerText = "El caracter " + entrada + " en código ASCII es: " + codigo;
+    document.getElementById("car-bin").value = codigo;
+}
+
+
+function abinario() {
     
-}//DOM
-
-
-function abinario(){
-    let cod = codigo;
-    for (let i = 7; i>=0 ;i--){
-        if (2 ** i<= cod){
-            binario=binario+"1";
-            cod=cod-2**i;            
-        }else{
+    let cod = document.getElementById("car-bin").value;
+    binario = ""; 
+    for (let i = 7; i >= 0; i--) {
+        if (2 ** i <= cod) {
+            binario = binario + "1";
+            cod = cod - 2 ** i;
+        } else {
             binario = binario + "0";
         }
     }
-    document.getElementById("sal").innerText="El caracter "+entrada+" en binario es:" +binario;
-    document.getElementById("decima").value=binario;
-
+    document.getElementById("sal").innerText = "El caracter " + entrada + " en binario es: " + binario;
+    document.getElementById("decima").value = binario;
 }
 
-function borrartodo (){
-    entrada = document.getElementById("entrada").value=""; // borrar todo lo que se puso en la entrada
-}
 
-function ejecutartton (){
+function ejecutartton() {
+    let tbin = document.getElementById("decima").value;
     let caracter = "";
-    let suma =0;
-    for (let vc = 7 ; vc >=0;vc--){
-        if (entrada[7-vc]=="1");
-        suma=suma+2**vc;
+    let suma = 0;
+    for (let vc = 7; vc >= 0; vc--) {
+        if (tbin[7 - vc] == "1") {
+            suma = suma + 2 ** vc;
+        }
     }
-    document.getElementById("").value=binario;
+    caracter = String.fromCharCode(suma);
+    document.getElementById("resultado-caracter").value="";
+    document.getElementById("resultado-caracter").innerText = "El binario " + tbin + " en caracter es: " + caracter;
+}
+
+// Función para borrar el contenido de los campos
+function borrartodo() {
+    document.getElementById("entrada").value = "";
+    document.getElementById("car-bin").value = "";
+    document.getElementById("decima").value = "";
+    document.getElementById("salida").innerText = "";
+    document.getElementById("sal").innerText = "";
+    document.getElementById("resultado-caracter").innerText = "";
+}
+
+// Función para borrar el campo de entrada de binario
+function quitartodo() {
+    document.getElementById("car-bin").value = "";
+    document.getElementById("sal").innerText = "";
 }
